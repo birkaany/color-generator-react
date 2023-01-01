f1dc6f57c860506d0122c82f72e62741a339857b
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-01-01 23:33:07 +0300

    style: fix saved list item width

--- src/components/PaletteListItem.jsx
+++ src/components/PaletteListItem.jsx
@@ -16,7 +16,7 @@ export default function PaletteListItem({ savedPaletteColor, setSavedColor, id }
   }
   return (
     <li className="group flex relative">
-      <div className="opacity-0 absolute flex justify-center items-center gap-10 w-full h-full top-0 left-0 z-20 bg-gray-300 transition-opacity group-hover:opacity-100 ">
+      <div className="opacity-0 absolute flex justify-center items-center gap-10 w-full h-full top-0 left-0 z-20 bg-gray-300 transition-opacity duration-300 group-hover:opacity-100 ">
         <button onClick={copyPaletteColor}>
           <BiCopy />
         </button>
@@ -25,7 +25,7 @@ export default function PaletteListItem({ savedPaletteColor, setSavedColor, id }
         </button>
       </div>
       {savedPaletteColor.map((color, index) => {
-        return <div key={index} style={{ background: `rgb(${color})` }} className="w-10 h-10"></div>;
+        return <div key={index} style={{ background: `rgb(${color})` }} className="w-full h-10"></div>;
       })}
     </li>
   );
